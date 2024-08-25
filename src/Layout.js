d227cf799ddf524955d02eca8abe16f26540e5e4
= <=>
2024-08-25 18:17:21 +1000

    Test layout change

--- src/Layout.js
+++ src/Layout.js
@@ -2,6 +2,25 @@ import React, { useState, useEffect, useRef } from "react";
 import classes from "./Layout.module.css";
 
 function Layout({ children }) {
+  const [showSettingsModal, setShowSettingsModal] = useState(false);
+
+  const toggleSettingsModal = () => {
+    setShowSettingsModal((showSettingsModal) => !showSettingsModal);
+  };
+
+  useEffect(() => {
+    let timer;
+    if (showSettingsModal) {
+      timer = setTimeout(() => {
+        setShowSettingsModal(false);
+      }, 20000);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [showSettingsModal]);
+
   return (
     <div className={classes.layout}>
       <div className={classes["background-video-container"]}>
@@ -13,6 +32,21 @@ function Layout({ children }) {
           muted
           className={classes["background-video"]}
         />
+      </div>
+
+      <div className={classes["settings-container"]}>
+        <button className={classes["settings-icon"]} onClick={toggleSettingsModal}>
+          &#9881;
+        </button>
+        {showSettingsModal && (
+          <div className={classes["settings-modal"]}>
+            <select className={classes["voice-dropdown"]}>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
+        )}
       </div>
 
       <div className={classes["content-container"]}>{children}</div>
